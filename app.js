a5efd6971af3f36a2774517deb4b0c10c09c473f
vietthom <thomle0418@gmail.com>
2022-12-04 19:53:06 -0700

    App.js

---
+++ app.js
@@ -0,0 +1,56 @@
+const settingsButton = document.querySelector('.settings');
+const startButton = document.querySelector('.start');
+const seconds = document.querySelector('.seconds > input[type=text]');
+const minutes = document.querySelector('.minutes > input[type=text]');
+const ring = document.querySelector('.ring');
+
+let startTime =0 ;
+let timer =null;
+let running =false;
+let originalMinutes = 0;
+let originalSeconds = 0;
+
+startButton.addEventListener('click', () => {
+    if(!running){
+        startTimer();
+    }else if(running){
+        pauseTimer();
+    }
+});
+
+const startTimer = () => {
+    running = true;
+    startButton.innerText = 'Pause';
+    startTime = Date.now();
+    const secondsValue = parseInt(seconds.value);
+    const minutesValue = parseInt(minutes.value);
+    totalSeconds = secondsValue + minutesValue * 60;
+    setInterval(() => {
+        console.log("Intervalling")
+        const currentTime= Date.now();
+        const diff = currentTime - startTime;
+        const secondsLeft = totalSeconds - Math.floor(diff/1000);
+        const minutesLeft = Math.floor(secondsLeft/60);
+        seconds.value = padNumber(secondsLeft);
+        minutes.value = padNumber(minutesLeft);
+
+        if(secondsLeft === 0 && minutesLeft === 0) {
+            finishTimer();
+        }
+    }, 1000)
+}
+
+const pauseTimer = () => {
+
+}
+
+const padNumber = (number) => {
+    if(number < 10) {
+        return "0" + number;
+    }
+    return number;
+}
+
+const finishTimer = () => {
+    
+}
